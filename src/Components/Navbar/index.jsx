956c16daa3d58da3173f606aebfe085e8653fca8
Th3g4ntl3m4n <jhernandez.sgsi@gmail.com>
2023-06-08 18:48:21 -0500

    product-detail

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { NavLink } from "react-router-dom"
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 
 import { ShoppingCartContext } from "../../Context"
 
@@ -113,8 +114,9 @@ const Navbar = () => {
                         SingIn
                     </NavLink>
                 </li>
-                <li>
-                    CAR {context.count}
+                <li className="flex items-center">
+                <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon> 
+                    <div className="">{context.count}</div>
                 </li>
                 
             </ul>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,19 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
   const [count, setCount] = useState(0)
-  
+  const [isProductDetailOpen, setProductDetailOpen] = useState(false)
+  const openProductDetail = () => setProductDetailOpen(true)
+  const closeProductDetail = () => setProductDetailOpen(false)
+
+
 
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen
     }}>
       {children}
     </ShoppingCartContext.Provider>
